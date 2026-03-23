bdc984f55891af5d99601f46d4304bd8843ad48d
kendranoneman <knoneman@andrew.cmu.edu>
2026-03-23 10:43:45 -0400

    not sure if i want to keep this here but oh well

---
+++ utils/rsc_bin_vectors.m
@@ -0,0 +1,22 @@
+function [x,y,c] = rsc_bin_vectors(Rtbl,Tclust,binID)
+
+% attach binIDs to cluster table
+Tclust.binID = binID;
+
+% build lookup keys
+key_clust = strcat(string(Tclust.sess_name),"_",string(Tclust.probe_index),"_",string(Tclust.cluster_id));
+key_n1 = strcat(string(Rtbl.sess_name),"_",string(Rtbl.probe_index),"_",string(Rtbl.n1_clust));
+key_n2 = strcat(string(Rtbl.sess_name),"_",string(Rtbl.probe_index),"_",string(Rtbl.n2_clust));
+
+% find matching clusters
+[~,loc1] = ismember(key_n1,key_clust);
+[~,loc2] = ismember(key_n2,key_clust);
+
+% bin IDs for each neuron in the pair
+x = Tclust.binID(loc1);
+y = Tclust.binID(loc2);
+
+% rsc values
+c = vertcat(Rtbl.rsc{:});
+
+end
